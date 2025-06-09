ac12f716c5aebb4c1cbe7af741967edd3079c41f
WillamesCampos <willwjccampos@gmail.com>
2025-06-09 10:48:31 -0300

    feature: Adicionando verificação de permissões nas rotas

---
+++ app/permissions.py
@@ -0,0 +1,40 @@
+from rest_framework import permissions
+
+
+class GlobalDefaultPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        model_permission_codename = self.__get_model_permission_codename(
+            method=request.method,
+            view=view
+        )
+
+        if not model_permission_codename:
+            return False
+
+        return request.user.has_perm(model_permission_codename)
+
+    def __get_action_suffix(self, method):
+        method_actions = {
+            'GET': 'view',
+            'POST': 'add',
+            'PUT': 'change',
+            'PATCH': 'change',
+            'DELETE': 'delete',
+            'OPTIONS': 'view',
+            'HEAD': 'view'
+        }
+
+        return method_actions.get(method, '')
+
+    def __get_model_permission_codename(self, method,  view):
+
+        try:
+            model_name = view.queryset.model._meta.model_name
+            app_label = view.queryset.model._meta.app_label
+            action = self.__get_action_suffix(method=method)
+
+        except AttributeError:
+            return None
+
+        return f'{app_label}.{action}_{model_name}'
--- app/settings.py
+++ app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from datetime import timedelta
 import os
 from pathlib import Path
 
@@ -146,4 +147,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
