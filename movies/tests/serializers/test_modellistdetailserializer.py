f0e71a4412904339ccd44665d5a66fc1689f1e0f
WillamesCampos <willwjccampos@gmail.com>
2025-07-30 17:03:30 -0300

    tests: Test coverage to MovieListDetailSerializer class

---
+++ movies/tests/serializers/test_modellistdetailserializer.py
@@ -0,0 +1,35 @@
+from datetime import date
+
+import pytest
+
+from actors.serializers import ActorSerializer
+from movies.factories import MovieFactory
+from movies.serializers import MovieListDetailSerializer
+
+
+@pytest.fixture
+def movie_factory():
+    return MovieFactory()
+
+
+@pytest.mark.django_db
+class TestMovieListDetailSerializer:
+    def setup_method(self):
+        self.serializer = MovieListDetailSerializer
+
+    def test_movie_list_detail_serializer_valid_data(self, movie_factory):
+        movie = movie_factory
+        movie.release_date = date(year=2010, month=7, day=16)
+        serializer = self.serializer(instance=movie)
+
+        validated = serializer.data
+
+        assert validated['id'] == movie.id
+        assert validated['title'] == movie.title
+        assert validated['genre']['id'] == movie.genre.id
+        assert validated['release_date'] == movie.release_date.strftime('%Y-%m-%d')
+        assert validated['resume'] == movie.resume
+        assert isinstance(validated['actors'], list)
+
+        actors = list(map(lambda actor: ActorSerializer(actor).data, movie.actors.all()))
+        assert validated['actors'] == actors
