4d14bdba96227d58c03e22b805ffffbaa0434b8e
WillamesCampos <willwjccampos@gmail.com>
2025-08-14 15:15:15 -0300

    chore: set BaseModel as main model with default fields and any model should inherit this model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth import get_user_model
 from django.db import models
 
@@ -5,11 +7,11 @@ User = get_user_model()
 
 
 class BaseModel(models.Model):
-    uuid = models.UUIDField(primary_key=True)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4())
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
-    updated_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='created_by_%(class)s')
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name='updated_by_%(class)s')
 
     class Meta:
         abstract = True
